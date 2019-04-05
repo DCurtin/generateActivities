f0cfe8ae118c468f27ab767792435b0fe0a0b317
Dalton Curtin <dcurtin@midland.local>
2019-04-05 12:39:53 -0400

    Adding copied over code for gen activities html and js.

--- lwc/generateActivities/generateActivities.js
+++ lwc/generateActivities/generateActivities.js
@@ -1,3 +1,204 @@
-import { LightningElement } from 'lwc';
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+import { LightningElement, track, wire} from 'lwc';
+import generateActivitiesAction from '@salesforce/apex/CreateActivitiesController.generateActivitiesAction';
+import insertActivitiesAction from '@salesforce/apex/CreateActivitiesController.insertActivitiesAction';
+import insertActivitiesActionBatch from '@salesforce/apex/CreateActivitiesController.insertActivitiesActionBatch';
 
-export default class GenerateActivities extends LightningElement {}
+export default class generateActivities extends LightningElement {
+    description="";
+    subject="";
+    task="";
+
+    //@wire(generateActivitiesAction, {csvBlob:"$csvBlob"})
+    csvBlob = null;
+    //@wire(generateActivitiesAction, { csvBlob: '$csvBlob' })
+    @track tasks;
+    @track statusMessage;// = 'Hello, please attach a csv.';
+    @track error;
+    
+    @track hideAttach;
+    @track hideInsert;
+
+    STATES = {
+        INIT:           'init',
+        SPLIT:          'split',
+        GENERATETASKS:  'generateTasks',
+        PROMPTINSERT:   'promptInsert',
+        INSERTING:      'inserting',
+        COMPLETE1k:     'complete1k',
+        COMPLETE:       'complete'
+    }
+    //currentState = STATES.INIT;
+
+    //get subject(){ return this.task.data ? getSObjectValue(this.task.data, SUBJECT_FIELD) : '';}
+    constructor()
+    {
+        super();
+        this.stateTransition(this.STATES.INIT);
+    }
+
+    insertTasks(){
+        /*var tasks = JSON.stringify(this.tasks);
+        console.log(tasks);*/
+        //this.stateTransition(this.STATES.COMPLETE1k);
+        this.stateTransition(this.STATES.INSERTING);
+
+        if(this.tasks.length > 1000)
+        {
+            insertActivitiesActionBatch({ tasks: this.tasks })
+            .then(function(result)
+            {
+                this.stateTransition(this.STATES.COMPLETE);
+            }.bind(this))
+            .catch(error => {
+                console.log(error);
+                alert(error);
+            });
+        }else
+        {
+            insertActivitiesAction({ tasks: this.tasks })
+            .then(function(result)
+            {
+                this.stateTransition(this.STATES.COMPLETE);
+            }.bind(this))
+            .catch(error => {
+                console.log(error);
+                alert(error);
+            });
+
+        }
+    }
+
+    fileParser(event){
+        var reader = new FileReader();
+        this.stateTransition(this.STATES.SPLIT);
+
+        const ufiles = event.detail.files;
+
+        reader.readAsText(ufiles[0], "UTF-8");
+        reader.onload = function (evt) {
+            var csv = evt.target.result;
+            var rows = csv.split("\n");
+            var invokeCallArgument = null;
+            var activitiesWrapper = null;
+            this.csvBlob = rows;
+
+            
+            
+        
+            activitiesWrapper = 
+            {
+                csvLines: rows,
+                options: {someVar: "1",
+                        somveVar2: "2"}
+            };    
+        
+            this.stateTransition(this.STATES.GENERATETASKS);
+            //var row;
+            //console.log("EVT FN");
+            //console.log('@@@ csv file contains'+ csv);
+            
+            //console.log("split successful");
+            //this.greeting = row;
+            
+            //this.greeting ='generating tasks, please wait.'
+            invokeCallArgument = { 
+                
+                csvBlob: this.csvBlob }
+            generateActivitiesAction({activities: activitiesWrapper})
+            .then(function(result)
+            {
+                this.stateTransition(this.STATES.PROMPTINSERT);
+                this.tasks=result;
+            }.bind(this))
+            .catch(error => {
+                console.log(error);
+                alert(error);
+                //this.error = error;
+            });
+            //var result = helper.CSV2JSON(component,csv);
+            //console.log('@@@ result = ' + result);
+            //console.log('@@@ Result = '+JSON.parse(result));
+            //helper.CreateAccount(component,result);
+            
+        }.bind(this)
+    }
+
+    stateTransition(state)
+    {
+        switch(state)
+        {
+            case this.STATES.INIT:
+            //set initial state clearing tasks/csvBlob
+            //and showing attach/hiding insert
+            //and initial message
+
+            this.tasks = null;
+            this.csvBlob = null;
+
+            this.hideAttach=false;
+            this.hideInsert=true;
+
+            this.statusMessage='Hello, Please attach a csv.';
+
+            break;
+
+            case this.STATES.SPLIT:
+            //hide attach
+            //change message
+
+            this.hideAttach=true;
+            this.hideInsert=true;
+            
+            this.statusMessage='Splitting CSV please wait...';
+            break;
+
+            case this.STATES.GENERATETASKS:
+            //change message
+
+            this.statusMessage='Generating activities please wait...';
+            break;
+
+            case this.STATES.PROMPTINSERT:
+            //show attach
+            //show insert
+            //prompt to insert current tasks for start over
+
+            this.hideAttach=false;
+            this.hideInsert=false;
+
+            this.statusMessage='Would you like to insert these records or insert a different set?';
+            break;
+
+            case this.STATES.INSERTING:
+            //hide attach
+            //hide insert
+            //change message
+
+            this.hideAttach=true;
+            this.hideInsert=true;
+
+            this.statusMessage='Inserting Docs';
+            break;
+
+            case this.STATES.COMPLETE1k:
+            //if count > 1k notify user that a batch job started
+            //they will receive and email when finished
+
+            this.statusMessage='You are creating over 1000 records, you will be emailed when the job is finished. Note this can take awhile.';
+            break;
+
+            case this.STATES.COMPLETE:
+            //notify user inserts are complete
+
+            this.statusMessage='Inserts Complete!';
+            break;
+
+            default:
+            //set default
+        }
+    }
+
+
+}
